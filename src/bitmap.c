aa3b8da72be2ee4ea0e5c00d2cc3f179ace51e9a
Spike Grobstein <me@spike.cx>
2018-06-26 21:31:26 -0700

    more WIP. why is this segfaulting.

--- src/bitmap.c
+++ src/bitmap.c
@@ -18,7 +18,7 @@ int main() {
 
   fclose(infile);
 
-  nrt_tile *tile = &chr->tile[0];
+  nrt_tile *tile = &chr->tile[1];
   nrt_tile_bitmap *bitmap = NRT_TILE_BITMAP_ALLOC;
   nrt_tile_to_bitmap(tile, bitmap);
 
@@ -39,24 +39,14 @@ int main() {
   }
 
   // set up the palette.
-  png_color *palette = (png_color*)png_malloc(png_ptr, 4 * sizeof(png_color));
-  png_color *c = NULL;
-  c = &palette[0];
-  c->red = 0;
-  c->green = 0;
-  c->blue = 0;
-  c = &palette[1];
-  c->red = 255;
-  c->green = 0;
-  c->blue = 0;
-  c = &palette[2];
-  c->red = 255;
-  c->green = 255;
-  c->blue = 0;
-  c = &palette[3];
-  c->red = 0;
-  c->green = 0;
-  c->blue = 255;
+  static const png_color palette[] = {
+    {0x00, 0x00, 0x00},
+    {0xFF, 0x00, 0x00},
+    {0xFF, 0xFF, 0x00},
+    {0x00, 0x00, 0xFF}
+  };
+
+  png_byte *row = (png_byte*)malloc(2 * sizeof(png_byte));
 
   png_init_io(png_ptr, outfile);
 //png_set_write_status_fn(png_ptr, write_row_callback);
@@ -64,36 +54,40 @@ int main() {
   png_set_IHDR(png_ptr, info_ptr, 8, 8, 2, PNG_COLOR_TYPE_PALETTE, PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
   png_set_PLTE(png_ptr, info_ptr, palette, 4);
 
-  png_bytep row = NULL;
-  row = (png_bytep) malloc(2 * sizeof(png_byte));
-
   png_write_info(png_ptr, info_ptr);
 
   int i;
   int j;
+  int idx = 0;
   for (i = 0; i < 8; i++) {
     // now that we're on the row, we have to write the binary index color to the row
     row[0] = 0;
     row[1] = 0;
 
     for (j = 0; j < 8; j++) {
-      if (j < 3) {
+      if (j <= 3) {
+        idx = 0;
         // first byte
-        row[0] += bitmap->pixels[i * 8 + j];
-        row[0] = row[0] << 2;
       } else {
+        idx = 1;
         //second byte
-        row[1] += bitmap->pixels[i * 8 + j];
-        row[1] = row[1] << 2;
       }
+      printf("Writing pixel:%d\n", i * 8 + j);
+      fflush(stdout);
+      row[idx] = row[idx] << 2;
+      row[idx] += bitmap->pixels[i * 8 + j];
     }
 
     png_write_row(png_ptr, row);
   }
 
   png_write_end(png_ptr, NULL);
-  fclose(outfile);
 
+  if (info_ptr != NULL) png_free_data(png_ptr, info_ptr, PNG_FREE_ALL, -1);
+	if (png_ptr != NULL) png_destroy_write_struct(&png_ptr, (png_infopp)NULL);
+  free(row);
+
+  fclose(outfile);
 
   printf("hello\n");
 }
