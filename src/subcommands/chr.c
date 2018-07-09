37851f0a3b956c1c24437d7fc75ac2f37fac8b16
Spike Grobstein <me@spike.cx>
2018-07-08 22:40:13 -0700

    added the CLI interface for replacing CHR banks.

--- src/subcommands/chr.c
+++ src/subcommands/chr.c
@@ -33,17 +33,16 @@ void subcommand_chr_extract(int argc, char **argv) {
   int bank_index = 0;
   char outfile_path[1024] = "";
 
-  printf("current: %s\n", argv[0]);
-
   int ch;
 
-  while ((ch = getopt_long(argc, argv, "f:", longopts, NULL)) != -1) {
+  while ((ch = getopt_long(argc, argv, "f:w:", longopts, NULL)) != -1) {
     switch (ch) {
       case 'f':
         strcpy(format, optarg);
         break;
       case 'w':
         width_in_tiles = atoi(optarg);
+        break;
       case 0:
         printf("nothing\n");
         break;
@@ -154,7 +153,135 @@ void subcommand_chr_extract(int argc, char **argv) {
   exit(EXIT_SUCCESS);
 }
 
+/*
+ * replace a CHR bank from a raw file or a png.
+ * usage:
+ * nrt chr replace <rom> [ <options> ] <bank-index> <bank-file>
+ * options:
+ *  --format=<png|chr>
+ *  --outfile=<outfilepath>
+ */
 void subcommand_chr_replace(int argc, char **argv) {
-  fprintf(stderr, "Not yet implemented.\n");
-  exit(EXIT_FAILURE);
+  static struct option longopts[] = {
+    { "format", required_argument, NULL, 'f' },
+    { "outfile", required_argument, NULL, 'o' },
+    { NULL, 0, NULL, 0 }
+  };
+
+  char *infile_path = argv[0]; // don't shift the args
+  char *bank_index_str = NULL;
+
+  char format[32] = "";
+  int bank_index = 0;
+  char outfile_path[1024] = "";
+  char bankfile_path[1024] = "";
+  static char tempfile_path[] = "/tmp/nrt-rom.XXXXXX";
+
+  int ch;
+
+  while ((ch = getopt_long(argc, argv, "f:o:", longopts, NULL)) != -1) {
+    switch (ch) {
+      case 'f':
+        strcpy(format, optarg);
+        break;
+      case 'o':
+        strcpy(outfile_path, optarg);
+        break;
+      case 0:
+        printf("nothing\n");
+        break;
+      default:
+        fprintf(stderr, "Invalid argument: -%c\n", ch);
+        exit(EXIT_FAILURE);
+    }
+  }
+
+  argc -= optind;
+  argv += optind;
+
+  bank_index_str = *(argv++);
+  strcpy(bankfile_path, *(argv++));
+
+  bank_index = atoi(bank_index_str);
+
+  if (strlen(format) == 0) {
+    // figure out the format if it's not passed
+    const char *ext = filename_ext(bankfile_path);
+
+    if (strcmp(ext, "chr") == 0) {
+      strcpy(format, "chr");
+    } else if (strcmp(ext, "png") == 0) {
+      strcpy(format, "png");
+    } else {
+      fprintf(stderr, "Please supply a format with --format=<fmt>\n");
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  if (strlen(outfile_path) == 0) {
+    // clobber the existing ROM
+    strcpy(outfile_path, infile_path);
+  }
+
+  // now we want to read in the source file, and get the data we really want
+  // then we'll read in the rom, replace the bank and write it to a tempfile
+  // then move the tempfile to the final location
+
+  nrt_chrbank *chr_bank = NRT_CHR_ALLOC;
+
+  if (strcmp(format, "png") == 0) {
+    FILE *pngfile = fopen(bankfile_path, "r");
+    if (!pngfile) {
+      perror("Failed to open up bank file");
+      exit(EXIT_FAILURE);
+    }
+
+    nrt_tile_bitmap *tiles = (nrt_tile_bitmap*)malloc(NRT_CHR_TILE_COUNT * sizeof(nrt_tile_bitmap));
+
+    if (! nrt_png_to_tiles(pngfile, tiles)) {
+      fprintf(stderr, "Failed to read in PNG file: %s\n", bankfile_path);
+      fclose(pngfile);
+      exit(EXIT_FAILURE);
+    }
+
+    // convert from bitmap to tiles in bank.
+    nrt_tile *raw_tile = NRT_TILE_ALLOC;
+    int i;
+    for(i = 0; i < NRT_CHR_TILE_COUNT; i++) {
+      nrt_bitmap_to_tile(&tiles[i], raw_tile);
+      memcpy(&chr_bank->tile[i], raw_tile, NRT_TILE_SIZE);
+    }
+
+    free(raw_tile);
+    free(tiles);
+  } else if (strcmp(format, "chr") == 0) {
+    printf("Not implemented\n");
+    exit(EXIT_FAILURE);
+  }
+
+  // ok, let's read in the rom file, and replace the chr
+  FILE *romfile = fopen(infile_path, "r");
+
+  if (!romfile) {
+    perror("Failed to open rom file for reading");
+    exit(EXIT_FAILURE);
+  }
+
+  nrt_rom *rom = nrt_read_rom_from_file(romfile);
+  fclose(romfile);
+
+  nrt_chr_listitem *bank_listitem = nrt_chr_listitem_at(rom->chrs, bank_index);
+  free(bank_listitem->chr);
+  bank_listitem->chr = chr_bank;
+
+  int fd = mkstemp(tempfile_path);
+  FILE *tempfile = fdopen(fd, "w");
+
+  nrt_rom_write_to_file(rom, tempfile);
+
+  fclose(tempfile);
+  nrt_rom_free(rom);
+
+  rename(tempfile_path, outfile_path);
+
 }
