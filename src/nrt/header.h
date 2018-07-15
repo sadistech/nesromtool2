4015ae75d0edca367579a3a0b6097fdb436beae8
Spike Grobstein <me@spike.cx>
2018-07-15 15:41:22 -0700

    filled out the header fields
    this includes the mapper data and all that.

--- src/nrt/header.h
+++ src/nrt/header.h
@@ -11,11 +11,45 @@
 
 #define NRT_HEADER_ALLOC (nrt_header*)malloc(sizeof(nrt_header))
 
+typedef enum tv_system {
+  tv_system_ntsc = 0,
+  tv_system_pal = 1
+} tv_system;
+
 typedef struct nrt_header {
   char magic_word[NRT_MAGIC_WORD_SIZE];
   unsigned char prg_count;
   unsigned char chr_count;
-  unsigned char the_rest[10];
+
+  // flags 6
+  unsigned char mapper_nybble_lower : 4;
+  unsigned char ignore_mirroring_control : 1;
+  unsigned char trainer_present : 1;
+  unsigned char persistent_memory : 1;
+  unsigned char mirror_mode : 1;
+
+  // flags 7
+  unsigned char mapper_nybble_upper : 4;
+  unsigned char flag_format : 2;
+  unsigned char playchoice : 1;
+  unsigned char vs_unisystem : 1;
+
+  // flags 8
+  unsigned char prg_ram_size;
+
+  // flags 9
+  unsigned char reserved : 7;
+  unsigned char tv_system : 1;
+
+  // flags 10
+  unsigned char unknown_1 : 2;
+  unsigned char bus_conflicts : 1;
+  unsigned char prg_ram_absent : 1;
+  unsigned char unknown_2 : 2;
+  unsigned char tv_system_2 : 2; // 00 = NTSC, 10 = PAL, 01/11 = both?
+
+  // zero filled
+  unsigned char the_rest[5];
 } nrt_header;
 
 nrt_header* nrt_header_create(int prg_count, int chr_count);
--- src/subcommands/info.c
+++ src/subcommands/info.c
@@ -58,10 +58,50 @@ void subcommand_info(int argc, char **argv) {
     "Valid: OK\n"
     "PRG Banks: %d\n"
     "CHR Banks: %d\n"
+    "\n"
+    "Mapper lower nybble: %d\n"
+    "Ignore mirroring control: %d\n"
+    "Trainer present: %d\n"
+    "Persistent memory: %d\n"
+    "Mirror mode: %d\n"
+    "\n"
+    "Mapper upper nybble: %d\n"
+    "Flag format: %d\n"
+    "Playchoice: %d\n"
+    "VS Unisystem: %d\n"
+    "\n"
+    "PRG RAM Size: %d\n"
+    "\n"
+    "TV System: %d\n"
+    "\n"
+    "Bus conflicts: %d\n"
+    "PRG RAM absent: %d\n"
+    "TV System (2): %d\n"
+    "\n"
     "Title: %s\n",
     opts->romfile_path,
     rom->prgs->count,
     rom->chrs->count,
+
+    rom->header->mapper_nybble_lower,
+    rom->header->ignore_mirroring_control,
+    rom->header->trainer_present,
+    rom->header->persistent_memory,
+    rom->header->mirror_mode,
+
+    rom->header->mapper_nybble_upper,
+    rom->header->flag_format,
+    rom->header->playchoice,
+    rom->header->vs_unisystem,
+
+    rom->header->prg_ram_size,
+
+    rom->header->tv_system,
+
+    rom->header->bus_conflicts,
+    rom->header->prg_ram_absent,
+    rom->header->tv_system_2,
+
     rom->title
   );
 
