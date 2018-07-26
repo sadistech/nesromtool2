c04c835f322f183188d222ed8c351e5e8456486d
Spike Grobstein <me@spike.cx>
2018-07-25 22:25:37 -0700

    added new filename_no_ext() function
    this is used to return a filename with no extension.

--- src/util.c
+++ src/util.c
@@ -2,9 +2,21 @@
 
 // from https://stackoverflow.com/questions/5309471/getting-file-extension-in-c
 const char *filename_ext(const char *filename) {
-    const char *dot = strrchr(filename, '.');
-    if(!dot || dot == filename) return "";
-    return dot + 1;
+  const char *dot = strrchr(filename, '.');
+  if(!dot || dot == filename) return "";
+  return dot + 1;
+}
+
+char *filename_no_ext(const char *filename) {
+  char *newfilename = (char*)calloc(1, strlen(filename + 1));
+  strcpy(newfilename, filename);
+  char *dot = strrchr(newfilename, '.');
+
+  if (!dot || dot == newfilename) return newfilename;
+
+  *dot = '\0';
+
+  return newfilename;
 }
 
 /* void vlog(int level, const char *format, ...) { */
--- src/util.h
+++ src/util.h
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 // TODO: use PATH_MAX from sys/limits.h here
 #define PATH_LENGTH 1024
@@ -20,6 +21,7 @@ enum LogLevel {
 // int verbosity = 0;
 
 const char *filename_ext(const char *filename);
+char *filename_no_ext(const char *filename);
 
 // void vlog(int level, const char *format, ...);
 
